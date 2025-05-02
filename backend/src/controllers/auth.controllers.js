d6195c725e00c8e72f30d97c2a3d59c9b1a37d72
brynsgtn <bryansuguitan17@gmail.com>
2025-05-02 13:45:30 -0500

    Implemented logout controller

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -139,8 +139,13 @@ export const login = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-    console.log("Logout");
-    res.send("Logout");
+    try {
+        res.cookie("jwt", "", { maxAge: 0 });
+        res.status(200).json({ message: "Logged out successfully" });
+    } catch (error) {
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 };
 
 export const updateProfile = (req, res) => {
