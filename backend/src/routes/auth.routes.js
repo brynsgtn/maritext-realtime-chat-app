e127913c22f00a5277273c31f2fda11c717745be
brynsgtn <bryansuguitan17@gmail.com>
2025-05-03 12:47:32 -0500

    Implemented protectRoute on update-profile-picture route

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -9,6 +9,7 @@ import {
     resetPassword,
     checkAuth, 
 } from "../controllers/auth.controllers.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
@@ -16,7 +17,7 @@ router.post("/signup", signup);
 router.post("/verify-email", verifyEmail);
 router.post("/login", login);
 router.post("/logout", logout);
-router.put("/update-profile-picture", updateProfilePicture);
+router.put("/update-profile-picture", protectRoute, updateProfilePicture);
 router.get("/forgot-password", forgotPassword);
 router.get("/reset-password", resetPassword);
 router.get("/check-auth", checkAuth);
