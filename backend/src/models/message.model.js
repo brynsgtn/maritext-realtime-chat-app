1b41739643d940f9cfb1acd6790ec786e8bf72be
brynsgtn <bryansuguitan17@gmail.com>
2025-05-06 13:29:04 -0500

    Added isRead and isDelivered

---
+++ backend/src/models/message.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+    {
+        senderId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        receiverId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        text: {
+            type: String,
+        },
+        image: {
+            type: String,
+        },
+        isDelivered: {
+            type: Boolean,
+            default: false,
+        },
+        deliveredAt: {
+            type: Date,
+            default: null,
+        },
+        isRead: {
+            type: Boolean,
+            default: false,
+        },
+        readAt: {
+            type: Date,
+            default: null,
+        },
+    },
+    { timestamps: true }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
