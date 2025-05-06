9aa82a9a6ace23fb70fb259ba5933a26dcf43487
brynsgtn <bryansuguitan17@gmail.com>
2025-05-06 12:41:56 -0500

    Implemented remove-contact route

--- backend/src/routes/contact.routes.js
+++ backend/src/routes/contact.routes.js
@@ -5,17 +5,19 @@ import {
     sendContactRequest,
     acceptContactRequest, 
     declineContactRequest,
-    getAllUserContacts
+    getAllUserContacts,
+    removeContact
 } from "../controllers/contact.controllers.js";
 import { protectRoute } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/users", getAllUsers);
-router.post("/send-contact-request", sendContactRequest);
+router.post("/send-contact-request", protectRoute, sendContactRequest);
 router.get("/get-contact-requests", protectRoute, getContactRequests);
 router.post("/accept-contact-request", protectRoute, acceptContactRequest);
 router.post("/decline-contact-request", protectRoute, declineContactRequest);
 router.get("/get-all-user-contacts", protectRoute, getAllUserContacts);
+router.delete("/remove-contact", protectRoute, removeContact);
 
 export default router;
