abba72c14922e5325618c4a8d67fa7af0ebef31f
brynsgtn <bryansuguitan17@gmail.com>
2025-05-06 14:25:49 -0500

    Implemented markLatestMessageAsRead route

--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { protectRoute } from "../middlewares/auth.middleware.js";
-import { getMessages, sendMessage } from "../controllers/message.controllers.js";
+import { getMessages, markLatestMessageAsRead, sendMessage } from "../controllers/message.controllers.js";
 
 const router = express.Router();
 
 
 router.get("/:id", protectRoute, getMessages);
 router.post("/send/:id", protectRoute, sendMessage);
+router.post("/read-message/:id", protectRoute, markLatestMessageAsRead);
 
 export default router;
