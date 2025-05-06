4aaeac788ffc04fcb3b07dc7ea13c5d3c3903aed
brynsgtn <bryansuguitan17@gmail.com>
2025-05-06 13:28:37 -0500

    Implemented getMessages controller

---
+++ backend/src/controllers/message.controllers.js
@@ -0,0 +1,22 @@
+import Message from "../models/message.model.js";
+
+export const getMessages = async (req, res) => {
+    try {
+        const { id: userToChatId } = req.params;
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            $or: [
+                { senderId: myId, receiverId: userToChatId },
+                { senderId: userToChatId, receiverId: myId },
+            ],
+        });
+
+        res.status(200).json(messages);
+    } catch (error) {
+        console.log("Error in getMessages controller: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+
