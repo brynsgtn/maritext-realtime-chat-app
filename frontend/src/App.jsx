495ebdbb448b05f1ff9a86b4ce86ac600abc4bde
brynsgtn <bryansuguitan17@gmail.com>
2025-05-09 15:08:19 -0500

    Frontend, tailwind css and daisy ui configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/Navbar"
+import EmailVerificationPage from "./pages/EmailVerificationPage"
+import ForgotPasswordPage from "./pages/ForgotPasswordPage"
+import HomePage from "./pages/HomePage"
+import LoginPage from "./pages/LoginPage"
+import ProfilePage from "./pages/ProfilePage"
+import ResetPasswordPage from "./pages/ResetPasswordPage"
+import SettingsPage from "./pages/SettingsPage"
+import SignUpPage from "./pages/SignUpPage"
+
+import { Routes, Route } from "react-router-dom"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/email-verification" element={<EmailVerificationPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
+      </Routes>
+    </div>
+
   )
 }
 
