22580a3a573ff0d1c2ab101b8df0e4d010cdf6df
brynsgtn <bryansuguitan17@gmail.com>
2025-05-10 14:24:01 -0500

    Configured route protections

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,9 +8,9 @@ import ResetPasswordPage from "./pages/ResetPasswordPage"
 import SettingsPage from "./pages/SettingsPage"
 import SignUpPage from "./pages/SignUpPage"
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
-import { useAuthStore} from "./store/useAuthStore.js";
+import { useAuthStore } from "./store/useAuthStore.js";
 import { useEffect } from "react"
 
 import { Loader } from "lucide-react";
@@ -18,33 +18,33 @@ import { Loader } from "lucide-react";
 
 function App() {
 
- const { authUser, checkAuth, isCheckingAuth} = useAuthStore();
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
- useEffect(() => {
-  checkAuth();
- },[checkAuth]);
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
 
- console.log(authUser);
+  console.log(authUser);
 
- if (isCheckingAuth && !authUser) return (
-  <div className="flex items-center justify-center h-screen">
-    <Loader className="size-10 animate-spin" />
-  </div>
-)
+  if (isCheckingAuth && !authUser) return (
+    <div className="flex items-center justify-center h-screen">
+      <Loader className="size-10 animate-spin" />
+    </div>
+  )
 
   return (
     <div>
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/email-verification" element={<EmailVerificationPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/email-verification" element={!authUser ? <EmailVerificationPage /> : <Navigate to="/" />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        <Route path="/forgot-password" element={!authUser ? <ForgotPasswordPage /> : <Navigate to="/" />} />
+        <Route path="/reset-password" element={!authUser ? <ResetPasswordPage /> : <Navigate to="/" />} />
       </Routes>
     </div>
 
