045314ef90d1003461fa83d24d52c3c2c875b6b7
brynsgtn <bryansuguitan17@gmail.com>
2025-05-17 17:19:15 -0500

    Implemented getUserContacts

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,40 @@
+import { create } from "zustand";
+import toast from "react-hot-toast";
+import { axiosInstance } from "../lib/axios";
+
+export const useChatStore = create((set) => ({
+    messages: [],
+    users: [],
+    contacts: [],
+    isContactsLoading: false,
+    selectedUser: null,
+    isUsersLoading: false,
+    isMessagesLoading: false,
+
+    getUserContacts: async () => {
+        set({ isContactsLoading: true });
+        try {
+            const res = await axiosInstance.get("/contacts/get-all-user-contacts");
+            set({ contacts: res.data.contacts });
+        } catch (error) {
+            toast.error(error?.response?.data?.message || error.message);
+        } finally {
+            set({ isContactsLoading: false });
+        };
+    },
+
+    getMessages: async (userId) => {
+        set({ isMessagesLoading: true });
+        try {
+            const res = await axiosInstance.get(`/messages/${userId}`);
+            set({ messages: res.data });
+        } catch (error) {
+            toast.error(error?.response?.data?.message || error.message);
+        } finally {
+            set({ isMessagesLoading: false });
+        };
+    },
+
+    // todo:optimize this later
+    setSelectedUser: (selectedUser) => set({ selectedUser })
+}));
