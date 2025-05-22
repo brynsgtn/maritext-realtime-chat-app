41cabed133bc3cd111504b5c2fba7397d0530f91
brynsgtn <bryansuguitan17@gmail.com>
2025-05-22 08:19:59 -0500

    Fixed skeleton array stack

--- frontend/src/components/skeletons/ModalSkeleton.jsx
+++ frontend/src/components/skeletons/ModalSkeleton.jsx
@@ -1,14 +1,23 @@
 const ModalSkeleton = () => {
+
+    const skeletonContacts = Array(3).fill(null);
+
     return (
-        <div className="flex w-52 flex-col gap-4">
-            <div className="flex items-center gap-4">
-                <div className="skeleton h-16 w-16 shrink-0 rounded-full"></div>
-                <div className="flex flex-col gap-4">
-                    <div className="skeleton h-4 w-55"></div>
-                    <div className="skeleton h-4 w-28"></div>
+        skeletonContacts.map((contact) => {
+            return (
+                <div className="flex flex-row">
+                    <div className="flex w-52 flex-col gap-4">
+                        <div className="flex items-center gap-4 my-3">
+                            <div className="skeleton h-16 w-16 shrink-0 rounded-full"></div>
+                            <div className="flex flex-col gap-4">
+                                <div className="skeleton h-4 w-55"></div>
+                                <div className="skeleton h-4 w-28"></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            )
+        })
     )
 }
 
