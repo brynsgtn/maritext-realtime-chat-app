90f2ee3fd641e982c7b871b0255d43891f5c336b
brynsgtn <bryansuguitan17@gmail.com>
2025-05-28 14:55:24 -0500

    Add online and offline label

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -1,5 +1,5 @@
 import { UserRoundXIcon, X } from "lucide-react";
-// import { useAuthStore } from "../store/useAuthStore";
+import { useAuthStore } from "../store/useAuthStore";
 import { useChatStore } from "../store/useChatStore";
 import { useEffect, useState } from "react";
 
@@ -7,7 +7,7 @@ import { useEffect, useState } from "react";
 const ChatHeader = () => {
   const { selectedUser, setSelectedUser, isRemovingContact, removeContact } = useChatStore();
   const [isModalOpen, setIsModalOpen] = useState(false);
-  // const { onlineUsers } = useAuthStore();
+  const { onlineUsers } = useAuthStore();
 
   const handleRemoveContact = (recipientId) => {
     removeContact(recipientId)
@@ -46,18 +46,21 @@ const ChatHeader = () => {
               <p className="text-xs text-base-content/70 truncate max-w-[180px] sm:max-w-[240px]">
                 {selectedUser.email}
               </p>
+              <p className="font-small text-xs text-base-content/70 truncate max-w-[180px] sm:max-w-[240px]">
+                {onlineUsers.includes(selectedUser._id) ? "Online" : "Offline"}
+              </p>
             </div>
           </div>
 
           {/* Close button */}
           <div className="flex flex-col items-center">
-          <button onClick={() => setSelectedUser(null)}>
-            <X />
-          </button>
-          <UserRoundXIcon
-            className="size-4 block md:hidden md:size-4 cursor-pointer text-error"
-            onClick={() => setIsModalOpen(true)}
-          />
+            <button onClick={() => setSelectedUser(null)}>
+              <X />
+            </button>
+            <UserRoundXIcon
+              className="size-4 block md:hidden md:size-4 cursor-pointer text-error"
+              onClick={() => setIsModalOpen(true)}
+            />
           </div>
         </div>
       </div>
