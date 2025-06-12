09e94d26cc0a1f9a9aa0cc183bec7073748530ad
brynsgtn <bryansuguitan17@gmail.com>
2025-06-12 11:57:12 -0500

    Add favicon and deployment configurations

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,9 @@ import authRoutes from "./routes/auth.routes.js";
 import contactRoutes from "./routes/contact.routes.js"
 import messageRoutes from "./routes/message.routes.js"
 import cors from "cors";
+
+import path from "path";
+
 import { app, server } from "./lib/socket.js"
 
 import { connectDB } from "./lib/db.js";
@@ -13,7 +16,8 @@ import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -27,6 +31,14 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/contacts", contactRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
     console.log(`🚀 Server Running on port ${PORT}`);
